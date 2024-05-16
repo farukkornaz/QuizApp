73165a6d1b3bc091b10fd0efa3ef04e7ad65ab54
faruk kornaz <fkornaz@hotmail.com>
2024-05-16 15:59:57 +0300

    kategori sayfasının controller'i uygulamaya baglandi

--- lib/bindings/essential_binding.dart
+++ lib/bindings/essential_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:quiz_test_app/controllers/auth_controller.dart';
+import 'package:quiz_test_app/controllers/category_screen_controller/category_screen_controller.dart';
 import 'package:quiz_test_app/controllers/question_controller.dart';
 import 'package:quiz_test_app/controllers/quiz_controller.dart';
 import 'package:quiz_test_app/controllers/user_controller.dart';
@@ -11,5 +12,6 @@ class EssentialBinding extends Bindings{
     Get.lazyPut<QuizController>(() => QuizController());
     Get.lazyPut<QuestionController>(() => QuestionController());
     Get.lazyPut<UserController>(() => UserController());
+    Get.lazyPut<CategoryScreenController>(() => CategoryScreenController());
   }
 }
